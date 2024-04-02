f56cc17bf096e344bc73c551c0fd0297cc9d995d
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-04-02 13:16:43 +0300

    Proje Oluşturuldu ve Database Bağlantıları Yapıldı

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternCapstone.Models
+{
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+    }
+}
