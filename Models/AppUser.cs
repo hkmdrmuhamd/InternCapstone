b9713c98c2b876dad34451b0f7815366b271f3e8
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-04-17 14:14:36 +0300

    User Profile Sayfası Yapıldı

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -6,8 +6,8 @@ namespace InternCapstone.Models
     public class AppUser : IdentityUser
     {
         public string? FullName { get; set; }
-        public string? DepartmentId { get; set; }
+        public string? DprtmntId { get; set; }
         public string? SubDivision { get; set; }
-        public List<SubDivision> SubDivisions { get; set; } = new List<SubDivision>(); // Kullanıcının çalıştığı bir veya birden fazla alt birim olabilir.
+        public List<SubDivision> SubDivisions { get; set; } = new List<SubDivision>();
     }
 }
